ac6a171bfa427723bbb955b11ba26697ad34918d
X7123M3-256 <X123M3-256@users.noreply.github.com>
2016-06-18 10:58:07 +0100

    Changed testing code\n

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 int main()
 {
 const char* filename="ObjData/BUTTFLY.DAT";
-\
+
 FILE* file=fopen(filename,"rb");
     if(file==NULL)
     {
@@ -38,23 +38,29 @@ error=small_scenery_decode(&scenery,&(object.chunk));
 
 
 object_t object2=object;
-//object2.flags=object.flags;
-//object2.checksum=object.checksum;
-//memcpy(object2.name,object.name,8);
+object2.flags=object.flags;
+object2.checksum=object.checksum;
+memcpy(object2.name,object.name,8);
 
 //uint8_t* data;uint32_t length;
 //chunk_decode(&(object.chunk),&data,&length);
 //chunk_encode(&(object2.chunk),data,length);
 
 
-/*
+
 error=small_scenery_encode(&scenery,&(object2.chunk));
     if(error!=ERROR_NONE)
     {
     printf("Error: %s\n",error_string(error));
     }
-*/
 
+file=fopen("BUTTFLY2.DAT","w");
+error=object_write(&object2,file);
+    if(error!=ERROR_NONE)
+    {
+    printf("Could not open file\n");
+    return 1;
+    }
 object_destroy(&object);
 return 0;
 }
