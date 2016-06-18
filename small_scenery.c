9254e60cf305316767cf911d7d9e8fa9d4bf6e20
X7123M3-256 <X123M3-256@users.noreply.github.com>
2016-06-18 14:56:52 +0100

    Rename files

---
+++ small_scenery.c
@@ -0,0 +1,126 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "dat.h"
+
+
+static error_t animation_indices_decode(animation_indices_t* animation_indices,uint8_t* data,uint32_t data_length,uint32_t* length)
+{
+uint32_t pos=0;
+    while(pos<data_length&&data[pos]!=0xFF)pos++;
+    if(pos>=data_length)return ERROR_PREMATURE_END_OF_CHUNK;
+animation_indices->num_indices=pos;
+animation_indices->indices=malloc(animation_indices->num_indices);
+memcpy(animation_indices->indices,data,animation_indices->num_indices);
+
+*length=pos+1;
+return ERROR_NONE;
+}
+static uint32_t animation_indices_get_encoded_length(animation_indices_t* animation_indices)
+{
+return animation_indices->num_indices+1;
+}
+static void animation_indices_encode(animation_indices_t* animation_indices,uint8_t* data)
+{
+memcpy(data,animation_indices->indices,animation_indices->num_indices);
+data[animation_indices->num_indices]=0xFF;
+}
+
+error_t small_scenery_decode(small_scenery_t* scenery,chunk_t* chunk)
+{
+uint8_t* data;
+uint32_t data_length;
+error_t error=chunk_decode(chunk,&data,&data_length);
+    if(error!=ERROR_NONE)return error;
+
+//Load header data
+    if(data_length<0x1C)
+    {
+    free(data);
+    return ERROR_PREMATURE_END_OF_CHUNK;
+    }
+scenery->flags=*((uint32_t*)(data+6));
+scenery->height=data[10];
+scenery->cursor_sel=data[11];
+scenery->build_fee=*((int16_t*)(data+12));
+scenery->remove_fee=*((int16_t*)(data+14));
+scenery->anim_delay=*((int16_t*)(data+20));
+scenery->anim_parameters=*((int16_t*)(data+22));
+scenery->anim_frames=*((int16_t*)(data+24));
+
+uint32_t pos=0x1C;
+
+//Load string table
+uint32_t length;
+error=string_table_decode(&(scenery->name),data+pos,data_length-pos,&length);
+    if(error!=ERROR_NONE)return error;
+pos+=length;
+
+//Load group info
+error=group_info_decode(&(scenery->group_info),data+pos,length-pos);
+    if(error!=ERROR_NONE)
+    {
+    string_table_destroy(&(scenery->name));
+    return error;
+    }
+pos+=16;
+
+//Load animation indices (if present)
+    if(scenery->flags&SMALL_SCENERY_ANIMDATA)
+    {
+    error=animation_indices_decode(&(scenery->animation_indices),data+pos,length-pos,&length);
+        if(error!=ERROR_NONE)
+        {
+        string_table_destroy(&(scenery->name));
+        return error;
+        }
+    pos+=length;
+    }
+//Load images
+error=image_list_decode(&(scenery->sprites),data+pos,data_length-pos);
+    if(error!=ERROR_NONE)
+    {
+    string_table_destroy(&(scenery->name));
+    return error;
+    }
+free(data);
+return ERROR_NONE;
+}
+error_t small_scenery_encode(small_scenery_t* scenery,chunk_t* chunk)
+{
+//Compute length of encoded data
+uint32_t name_table_length=string_table_get_encoded_length(&(scenery->name));
+uint32_t animation_indices_length=scenery->flags&SMALL_SCENERY_ANIMDATA?animation_indices_get_encoded_length(&(scenery->animation_indices)):0;
+uint32_t sprites_length=image_list_get_encoded_length(&(scenery->sprites));
+uint32_t length=0x2C+name_table_length+animation_indices_length+sprites_length;
+
+//Allocate memory
+uint8_t* data=malloc_or_die(length);
+memset(data,0,length);
+
+//Write header
+*((uint32_t*)(data+6))=scenery->flags;
+data[10]=scenery->height;
+data[11]=scenery->cursor_sel;
+*((int16_t*)(data+12))=scenery->build_fee;;
+*((int16_t*)(data+14))=scenery->remove_fee;
+*((int16_t*)(data+20))=scenery->anim_delay;
+*((int16_t*)(data+22))=scenery->anim_parameters;
+*((int16_t*)(data+24))=scenery->anim_frames;
+
+uint32_t pos=0x1C;
+string_table_encode(&(scenery->name),data+pos);
+pos+=name_table_length;
+group_info_encode(&(scenery->group_info),data+pos);
+pos+=16;
+    if(scenery->flags&SMALL_SCENERY_ANIMDATA)
+    {
+    animation_indices_encode(&(scenery->animation_indices),data+pos);
+    pos+=animation_indices_length;
+    }
+image_list_encode(&(scenery->sprites),data+pos);
+chunk_encode(chunk,ENCODING_RLE,data,length);
+free(data);
+return ERROR_NONE;
+}
---
+++ small_scenery.h
@@ -0,0 +1,64 @@
+#ifndef DAT_SMALL_SCENERY_H_INCLUDED
+#define DAT_SMALL_SCENERY_H_INCLUDED
+#include <stdint.h>
+
+enum //Small scenery flags
+{
+SMALL_SCENERY_FULL_TILE=0x00000001,
+SMALL_SCENERY_OFFSET=0x00000002,
+SMALL_SCENERY_FLAT=0x00000004,
+SMALL_SCENERY_RARROW=0x00000008,
+SMALL_SCENERY_ANIMATED=0x00000010,
+SMALL_SCENERY_GARDEN=0x00000020,
+SMALL_SCENERY_CAN_WATER=0x00000040,
+SMALL_SCENERY_DRAW2DIALOG=0x00000080,
+SMALL_SCENERY_DIAGONAL=0x00000100,
+SMALL_SCENERY_GLASS=0x00000200,
+SMALL_SCENERY_ENABLE_REMAP1=0x00000400,
+SMALL_SCENERY_FOUNTAIN1=0x00000800,
+SMALL_SCENERY_FOUNTAIN4=0x00001000,
+SMALL_SCENERY_CLOCK=0x00002000,
+SMALL_SCENERY_SWAMPGOO=0x00004000,
+SMALL_SCENERY_ANIMDATA=0x00008000,
+SMALL_SCENERY_DRAW2MAIN=0x00010000,
+SMALL_SCENERY_STACKABLE=0x00020000,
+SMALL_SCENERY_NO_WALLS=0x00040000,
+SMALL_SCENERY_ENABLE_REMAP_2=0x00080000,
+SMALL_SCENERY_NO_SUPPORTS=0x00100000,
+SMALL_SCENERY_DIALOG_FRAMES=0x00200000,
+SMALL_SCENERY_SMALL_COG=0x00400000,
+SMALL_SCENERY_HALF_TILE=0x01000000,
+SMALL_SCENERY_THREE_QUARTER_TILE=0x02000000,
+SMALL_SCENERY_REMAP_SUPPORTS=0x04000000,
+SMALL_SCENERY_POLE=0x08000000,
+}small_scenery_flags_t;
+
+//Small scenery optional data
+typedef struct
+{
+short num_indices;
+uint8_t* indices;
+}animation_indices_t;
+
+typedef struct //Small scenery
+{
+uint32_t flags;
+uint16_t anim_delay;
+uint16_t anim_parameters;
+uint16_t anim_frames;
+uint8_t height;
+uint8_t cursor_sel;
+int8_t build_fee;
+int8_t remove_fee;
+string_table_t name;
+animation_indices_t animation_indices;
+group_info_t group_info;
+image_list_t sprites;
+}small_scenery_t;
+
+
+error_t small_scenery_decode(small_scenery_t* small_scenery,chunk_t* chunk);
+error_t small_scenery_encode(small_scenery_t* small_scenery,chunk_t* chunk);
+
+void object_small_scenery_free();
+#endif // DAT_SMALL_SCENERY_H_INCLUDED
