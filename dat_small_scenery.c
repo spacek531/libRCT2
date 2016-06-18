6bca3c7215aa3ce3345889e088948d1340aa41a0
X123M3-256 <edward@knaphill.me.uk>
2016-06-18 11:07:48 +0100

    Fix bugs

--- dat_small_scenery.c
+++ dat_small_scenery.c
@@ -24,7 +24,7 @@ return animation_indices->num_indices+1;
 static void animation_indices_encode(animation_indices_t* animation_indices,uint8_t* data)
 {
 memcpy(data,animation_indices->indices,animation_indices->num_indices);
-data[animation_indices->num_indices+1]=0xFF;
+data[animation_indices->num_indices]=0xFF;
 }
 
 error_t small_scenery_decode(small_scenery_t* scenery,chunk_t* chunk)
@@ -33,10 +33,6 @@ uint8_t* data;
 uint32_t data_length;
 error_t error=chunk_decode(chunk,&data,&data_length);
     if(error!=ERROR_NONE)return error;
-
-FILE* file=fopen("DECOMPRESSED.DAT","w");
-fwrite(data,1,data_length,file);
-fclose(file);
 
 //Load header data
     if(data_length<0x1C)
@@ -91,14 +87,12 @@ error=image_list_decode(&(scenery->sprites),data+pos,data_length-pos);
 free(data);
 return ERROR_NONE;
 }
-
 error_t small_scenery_encode(small_scenery_t* scenery,chunk_t* chunk)
 {
 //Compute length of encoded data
 uint32_t name_table_length=string_table_get_encoded_length(&(scenery->name));
 uint32_t animation_indices_length=scenery->flags&SMALL_SCENERY_ANIMDATA?animation_indices_get_encoded_length(&(scenery->animation_indices)):0;
 uint32_t sprites_length=image_list_get_encoded_length(&(scenery->sprites));
-printf("%d\n",sprites_length);
 uint32_t length=0x2C+name_table_length+animation_indices_length+sprites_length;
 
 //Allocate memory
@@ -120,8 +114,13 @@ string_table_encode(&(scenery->name),data+pos);
 pos+=name_table_length;
 group_info_encode(&(scenery->group_info),data+pos);
 pos+=16;
-animation_indices_encode(&(scenery->animation_indices),data+pos);
-pos+=animation_indices_length;
+    if(scenery->flags&SMALL_SCENERY_ANIMDATA)
+    {
+    animation_indices_encode(&(scenery->animation_indices),data+pos);
+    pos+=animation_indices_length;
+    }
 image_list_encode(&(scenery->sprites),data+pos);
+chunk_encode(chunk,ENCODING_RLE,data,length);
+free(data);
 return ERROR_NONE;
 }
