a6a04ef58953f0a59fa72dba4ffce1e29d25ae3f
X7123M3-256 <X123M3-256@users.noreply.github.com>
2017-07-28 22:21:47 +0100

    Rename image_destroy

--- image.h
+++ image.h
@@ -16,5 +16,5 @@ void image_new(rct2_image_t* image,uint16_t width,uint16_t height,int16_t x_offs
 void image_copy(rct2_image_t* src,rct2_image_t* dst);
 void image_blit(rct2_image_t* dst,rct2_image_t* src,int16_t x_offset,int16_t y_offset);
 void image_write_bmp(rct2_image_t* image,FILE* file);
-void image_destroy(rct2_image_t* image);
+void rct2_image_destroy(rct2_image_t* image);
 #endif // IMAGE_H_INCLUDED
